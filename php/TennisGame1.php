68af750dc496ad229c9b14d00aa68b040a408d2c
Ioan Cristea <ioan.cristea@pitechnologies.ro>
2020-03-30 17:06:42 +0300

    Refactored TennisGame1.

--- php/TennisGame1.php
+++ php/TennisGame1.php
@@ -2,79 +2,85 @@
 
 class TennisGame1 implements TennisGame
 {
-    private $m_score1 = 0;
-    private $m_score2 = 0;
-    private $player1Name = '';
-    private $player2Name = '';
+    private $gameScore1 = 0;
+    private $gameScore2 = 0;
+    private $player1 = '';
+    private $player2 = '';
 
-    public function __construct($player1Name, $player2Name)
+    public function __construct($player1, $player2)
     {
-        $this->player1Name = $player1Name;
-        $this->player2Name = $player2Name;
+        $this->player1 = $player1;
+        $this->player2 = $player2;
     }
 
     public function wonPoint($playerName)
     {
         if ('player1' == $playerName) {
-            $this->m_score1++;
+            $this->gameScore1++;
         } else {
-            $this->m_score2++;
+            $this->gameScore2++;
         }
     }
 
     public function getScore()
     {
-        $score = "";
-        if ($this->m_score1 == $this->m_score2) {
-            switch ($this->m_score1) {
-                case 0:
-                    $score = "Love-All";
-                    break;
-                case 1:
-                    $score = "Fifteen-All";
-                    break;
-                case 2:
-                    $score = "Thirty-All";
-                    break;
-                default:
-                    $score = "Deuce";
-                    break;
-            }
-        } elseif ($this->m_score1 >= 4 || $this->m_score2 >= 4) {
-            $minusResult = $this->m_score1 - $this->m_score2;
-            if ($minusResult == 1) {
-                $score = "Advantage player1";
-            } elseif ($minusResult == -1) {
-                $score = "Advantage player2";
-            } elseif ($minusResult >= 2) {
-                $score = "Win for player1";
-            } else {
-                $score = "Win for player2";
-            }
-        } else {
-            for ($i = 1; $i < 3; $i++) {
-                if ($i == 1) {
-                    $tempScore = $this->m_score1;
-                } else {
-                    $score .= "-";
-                    $tempScore = $this->m_score2;
-                }
-                switch ($tempScore) {
-                    case 0:
-                        $score .= "Love";
-                        break;
-                    case 1:
-                        $score .= "Fifteen";
-                        break;
-                    case 2:
-                        $score .= "Thirty";
-                        break;
-                    case 3:
-                        $score .= "Forty";
-                        break;
-                }
-            }
+        if ($this->gameScore1 == $this->gameScore2) {
+            return $this->getEqualityScore();
         }
+
+        if ($this->gameScore1 >= 4 || $this->gameScore2 >= 4) {
+            return $this->getWinOrAdvantage();
+        }
+
+        return $this->getTempScore($this->gameScore1) . '-' . $this->getTempScore($this->gameScore2);
+    }
+
+    private function getEqualityScore(): string
+    {
+        if ($this->gameScore1 > 2) {
+            return "Deuce";
+        }
+
+        return $this->getTempScore($this->gameScore1) . "-All";
+    }
+
+    private function getWinOrAdvantage(): string
+    {
+        $minusResult = $this->gameScore1 - $this->gameScore2;
+        $result = $this->getResult($minusResult);
+        if ($minusResult > 0) {
+            return $result . "player1";
+        }
+
+        return $result . "player2";
+    }
+
+    private function getResult($difference): string
+    {
+        if (abs($difference) === 1) {
+            return "Advantage ";
+        }
+
+        return "Win for ";
+    }
+
+    private function getTempScore($score): string
+    {
+        switch ($score) {
+            case 0:
+                $score = "Love";
+                break;
+            case 1:
+                $score = "Fifteen";
+                break;
+            case 2:
+                $score = "Thirty";
+                break;
+            case 3:
+                $score = "Forty";
+                break;
+        }
+
         return $score;
     }
 }
