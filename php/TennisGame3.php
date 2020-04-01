654b547035878e0deb90080ac8b7649287457b1c
Ioan Cristea <ioan.cristea@pitechnologies.ro>
2020-04-01 08:08:28 +0300

    Refactored TennisGame3.

--- php/TennisGame3.php
+++ php/TennisGame3.php
@@ -2,38 +2,66 @@
 
 class TennisGame3 implements TennisGame
 {
-    private $p2 = 0;
-    private $p1 = 0;
-    private $p1N = '';
-    private $p2N = '';
+    private const POINTS = ["Love", "Fifteen", "Thirty", "Forty"];
+    private const DEUCE = "Deuce";
+    private const ALL = "All";
+    private const SEPARATOR = "-";
+    private $pointsPlayer2 = 0;
+    private $pointsPlayer1 = 0;
+    private $namePlayer1 = '';
+    private $namePlayer2 = '';
 
-    public function __construct($p1N, $p2N)
+    public function __construct($namePlayer1, $namePlayer2)
     {
-        $this->p1N = $p1N;
-        $this->p2N = $p2N;
+        $this->namePlayer1 = $namePlayer1;
+        $this->namePlayer2 = $namePlayer2;
     }
 
     public function getScore()
     {
-        if ($this->p1 < 4 && $this->p2 < 4 && !($this->p1 + $this->p2 == 6)) {
-            $p = array("Love", "Fifteen", "Thirty", "Forty");
-            $s = $p[$this->p1];
-            return ($this->p1 == $this->p2) ? "{$s}-All" : "{$s}-{$p[$this->p2]}";
-        } else {
-            if ($this->p1 == $this->p2) {
-                return "Deuce";
-            }
-            $s = $this->p1 > $this->p2 ? $this->p1N : $this->p2N;
-            return (($this->p1 - $this->p2) * ($this->p1 - $this->p2) == 1) ? "Advantage {$s}" : "Win for {$s}";
+        if ($this->pointsPlayer1 == $this->pointsPlayer2) {
+            return $this->getEqualsScore();
         }
+
+        if ($this->pointsPlayer1 > 3 || $this->pointsPlayer2 > 3) {
+            return $this->getWinOrAdvantage();
+        }
+
+        return $this->getPlayerScore($this->pointsPlayer1) .
+            self::SEPARATOR .
+            $this->getPlayerScore($this->pointsPlayer2);
+    }
+
+    private function getEqualsScore()
+    {
+        if ($this->pointsPlayer1 > 2) {
+            return self::DEUCE;
+        }
+        return $this->getPlayerScore($this->pointsPlayer1) . self::SEPARATOR . self::ALL;
+    }
+
+    private function getPlayerScore($player): string
+    {
+        return self::POINTS[$player];
+    }
+
+    private function getWinOrAdvantage(): string
+    {
+        $scoreDifference = $this->pointsPlayer1 - $this->pointsPlayer2;
+        $name = $scoreDifference > 0 ? $this->namePlayer1 : $this->namePlayer2;
+
+        if (abs($scoreDifference) == 1) {
+            return "Advantage {$name}";
+        }
+        return "Win for {$name}";
     }
 
     public function wonPoint($playerName)
     {
-        if ($playerName == "player1") {
-            $this->p1++;
+        if ($playerName == $this->namePlayer1) {
+            $this->pointsPlayer1++;
         } else {
-            $this->p2++;
+            $this->pointsPlayer2++;
         }
     }
 
